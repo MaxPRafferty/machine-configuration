5c3a168bfe99be8394a3fcf2181304fadf2092a2
maxprafferty <maxprafferty@gmail.com>
2015-10-12 16:08:27 -0400

    force pathogen install, updated setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 cp ./.vimrc ~/.vimrc
 #cp -R ./.vim ~/.vim
-cp -R ~/machine-configuration/.vim/colors/ ~/.vim/colors/
-
+cp -R ./.vim/colors/ ~/.vim/colors/
+cp -R ./.vim/autoload/ ~/.vim/autoload/
+cp ./.bash_profile ~/.bash_profile
