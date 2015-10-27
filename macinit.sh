e97ec7edb61cd436787acdbfaa02eab3bc181ec6
MaxPRafferty <MaxPRafferty@gmail.com>
2015-10-27 10:08:09 -0400

    added new mac initialization script

---
+++ macinit.sh
@@ -0,0 +1,6 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install brackets
+brew cask install spectacle
+brew cask install iterm2-nightly
