a58b384d9b9f46ce9d865483c5e7d0d4ecad2d1b
MaxPRafferty <MaxPRafferty@gmail.com>
2015-10-28 16:36:26 -0400

    added git, kaleidoscope to mac setup

--- macinit.sh
+++ macinit.sh
@@ -1,8 +1,12 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install npm
 brew install caskroom/cask/brew-cask
 brew cask install google-chrome
 brew cask install brackets
 brew cask install spectacle
 brew cask install iterm2-nightly
 brew cask install chrome-devtools
+brew install ctags
+brew install Caskroom/cask/kaleidoscope
 
