f25bb3a2f8187a7ddc4b4127fae9713bad54e730
Max Rafferty <maxprafferty@gmail.com>
2015-10-28 23:40:31 -0400

    added hg install

--- macinit.sh
+++ macinit.sh
@@ -7,6 +7,7 @@ brew cask install brackets
 brew cask install spectacle
 brew cask install iterm2-nightly
 brew cask install chrome-devtools
+brew install hg
 brew install ctags
 brew install Caskroom/cask/kaleidoscope
 
