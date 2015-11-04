7162793c59bc67fa90a8856c533470d250db6ce4
Max Rafferty <maxprafferty@gmail.com>
2015-11-04 10:33:06 -0500

    updated install true color vim for +clipboard, removed broken whitespace config

--- installtruecolorvim.sh
+++ installtruecolorvim.sh
@@ -2,8 +2,10 @@ CWD=$(pwd)
 rm -rf ~/truecolorvim
 hg clone https://bitbucket.org/ZyX_I/vim ~/truecolorvim
 cd ~/truecolorvim
-hg bookmark 24-bit-xterm
-./configure --with-features=huge --enable-rubyinterp --enable-termtruecolor  --enable-pythoninterp --disable-darwin --enable-gui=auto --enable-clipboard --with-x
+hg update 24-bit-xterm #from https://gist.github.com/othree/6087586
+#hg bookmark 24-bit-xterm #from https://www.reddit.com/r/vim/comments/2ozwe4/24_bit_vim_in_osx_iterm2_a_reality/
+(cd src && ./configure --with-features=huge --enable-termtruecolor --enable-pythoninterp  --enable-rubyinterp --enable-gui=auto )
+#also good config instructions: https://kowalcj0.wordpress.com/2013/11/19/how-to-compile-and-install-latest-version-of-vim-with-support-for-x11-clipboard-ruby-python-2-3/
 make && sudo make install
 cd $CWD
 unset CWD
