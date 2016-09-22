c03bfb959b5e194ae63607517baab1cfcb705a95
maxprafferty <maxprafferty@gmail.com>
2016-09-21 22:47:21 -0400

    elm init

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouComplet
 git clone git://github.com/ntpeters/vim-better-whitespace.git ~/.vim/bundle/vim-better-whitespace
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/CtrlP
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/lambdatoast/elm.vim.git ~/.vim/bundle/elm.vim
 git clone https://github.com/bling/vim-bufferline.git ~/.vim/bundle/bufferline
 git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
 git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
